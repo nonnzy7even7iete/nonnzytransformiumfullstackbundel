3d33e9dca3057ff2ed26a2aaa50de54f22031d88
Nonnzy <nonnzytransformium@gmail.com>
2025-09-28 15:46:02 +0000

    Mise à jour complète du projet

---
+++ src/lib/auth.ts
@@ -0,0 +1,34 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "database",
+    maxAge: 60 * 60 * 24 * 30,
+  },
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) session.user.id = user.id;
+      return session;
+    },
+    async redirect({ baseUrl }) {
+      return `${baseUrl}/dashboard`;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+};
