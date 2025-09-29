29e98b89265eace59f437982119f21d58d89c6d6
Nonnzy <nonnzytransformium@gmail.com>
2025-09-29 00:47:06 +0000

    Zy Bulding workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,13 +18,11 @@ export default function HomePage() {
 
   return (
     <main className="flex items-center justify-center min-h-screen bg-zinc-900">
-
       {/* Card principale */}
       <div className="w-[277px] h-[277px] p-[7px] bg-black/50 backdrop-blur-md border border-white/20 rounded-[7px] relative flex flex-col justify-center items-center shadow-lg">
-
         {/* Titre dans sa mini-card glassmorphique */}
-        <div className="absolute -top-6 bg-black/50 backdrop-blur-md border border-white/20 rounded-[7px] px-[7px] py-[7px]">
-          <h1 className="text-transparent font-bold text-xl bg-clip-text bg-gradient-to-r from-gray-300 to-gray-300 drop-shadow-sm text-center">
+        <div className="absolute -top-6 bg-black/70 backdrop-blur-md border border-white/20 rounded-[7px] px-[14px] py-[7px]">
+          <h1 className="text-transparent font-bold text-xl bg-clip-text bg-gradient-to-r from-gray-100 to-gray-700 drop-shadow-sm text-center">
             Nonnzytrasformium
           </h1>
         </div>
@@ -42,10 +40,10 @@ export default function HomePage() {
 
         {/* Texte explicatif collé en bas */}
         <p className="text-white/60 text-xs text-center font-thin absolute bottom-[2px] px-2 w-full">
-          Le seul moyen de continuer l'aventure avec nous est avec un compte Google / Continuer avec votre compte Google
+          Le seul moyen de continuer l'aventure avec nous est avec un compte
+          Google / Continuer avec votre compte Google
         </p>
       </div>
-
     </main>
   );
 }
