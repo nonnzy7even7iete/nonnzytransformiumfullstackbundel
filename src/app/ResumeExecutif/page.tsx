748d52293e22c8f131e0bc4b037404d2d9e90db2
Nonnzy <nonnzytransformium@gmail.com>
2025-11-21 16:19:17 +0000

    Zy Bulding workflow

--- src/app/ResumeExecutif/page.tsx
+++ src/app/ResumeExecutif/page.tsx
@@ -1,10 +1,15 @@
+import NavbarFront from "../components/NavbarFront"; // ajuste le chemin si nécessaire
+
 export default function ResumeExecutifPage() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-6">
-      <h1 className="text-4xl font-bold mb-6">Résumé Exécutif</h1>
-      <p className="text-gray-700 text-center max-w-xl">
-        Cette page est accessible sans authentification.
-      </p>
-    </main>
+    <>
+      <NavbarFront /> {/* Navbar affichée en haut */}
+      <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-6">
+        <h1 className="text-4xl font-bold mb-6">Résumé Exécutif</h1>
+        <p className="text-gray-700 text-center max-w-xl">
+          Cette page est accessible sans authentification.
+        </p>
+      </main>
+    </>
   );
 }
