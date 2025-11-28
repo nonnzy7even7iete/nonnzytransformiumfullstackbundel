5d9b50c2505f10a447f5a65b24b4c339ae140ac0
Nonnzy <nonnzytransformium@gmail.com>
2025-11-28 17:33:08 +0000

    Zy Bulding workflow

--- src/components/NavbarFront.tsx
+++ src/components/NavbarFront.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 import { FileText, LayoutDashboard } from "lucide-react";
 // 1. Import de la nouvelle icône
 import { IoAppsOutline } from "react-icons/io5";
+import { TextHoverEffect } from "./ui/TextHoverEffect";
 
 export default function NavbarFront() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -65,14 +66,7 @@ export default function NavbarFront() {
         <div className="max-w-7xl mx-auto px-4 py-2 grid grid-cols-3 items-center">
           {/* Colonne gauche : Logo */}
           <div>
-            <Link
-              href="/"
-              className="text-transparent bg-clip-text 
-                bg-gradient-to-r from-green-700 via-green-300 to-blue-400
-                font-semibold text-lg tracking-wide transition-all duration-300 hover:brightness-110"
-            >
-              Nonnzytr
-            </Link>
+            <TextHoverEffect text="Nonnzytr" />
           </div>
 
           {/* Colonne centrale : MENU 100% CENTRÉ */}
