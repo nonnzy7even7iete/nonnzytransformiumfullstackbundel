06dac015b7182ea1f4effaef0158f3b0decdd2cb
Nonnzy <nonnzytransformium@gmail.com>
2026-01-10 22:12:05 +0000

    Zy Bulding workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function HomePage() {
       <main
         className="relative flex flex-col md:flex-row items-center justify-center 
 min-h-screen bg-gradient-to-br from-zinc-700 via-black to-zinc-900 
-overflow-hidden px-4 md:px-8 py-10 gap-6 md:gap-0 pt-8 md:pt-20"
+overflow-hidden px-4 md:px-8 py-10 gap-6 md:gap-0 pt-8 md:pt-40"
       >
         {/* Fond dynamique */}
         <div className="absolute inset-0 z-0">
