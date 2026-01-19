d64a9b52af495b67c98ebbe79c5b70a47ade01c2
Nonnzy <nonnzytransformium@gmail.com>
2026-01-19 21:50:19 +0000

    Zy Bulding workflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./tailwind.css";
-// ⚠️ Vérifie bien que "export" n'est pas devant l'import ci-dessous
 import { Providers } from "@/components/Providers";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
@@ -15,8 +13,6 @@ export const metadata: Metadata = {
   title: "Nonnzy App",
   description: "Application fullstack avec Next.js",
 };
-
-// ⚠️ AUCUN AUTRE EXPORT NE DOIT ÊTRE PRÉSENT ICI
 
 export default function RootLayout({
   children,
