af1f976c024aa9b814bd203e7ed08e1bf77786f3
Nonnzy <nonnzytransformium@gmail.com>
2026-02-03 07:53:14 +0000

    Zy Bulding workflow

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-// On crée une fonction pour générer le client
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-// On définit le type pour TypeScript
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-// On attache Prisma au scope global pour éviter d'ouvrir
-// trop de connexions en mode développement
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
