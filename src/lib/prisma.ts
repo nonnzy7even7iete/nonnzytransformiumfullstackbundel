552252df00621dad802d2efe83f6e409f5f05d84
Nonnzy <nonnzytransformium@gmail.com>
2026-02-03 12:31:48 +0000

    Zy Bulding workflow

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  // On récupère l'URL
-  const url = process.env.DATABASE_URL;
-
-  // HACK DE SURVIE : Si l'URL est vide pendant le build, on donne une URL MongoDB
-  // valide syntaxiquement pour que Prisma ne plante pas le build.
-  const effectiveUrl =
-    url ||
-    "mongodb+srv://build-user:pass@localhost/temporary?retryWrites=true&w=majority";
-
-  return new PrismaClient({
-    datasourceUrl: effectiveUrl,
-  } as any);
+  // On laisse le constructeur VIDE.
+  // Prisma 7 lira automatiquement 'prisma.config.ts'.
+  return new PrismaClient();
 };
 
 const globalForPrisma = globalThis as unknown as {
