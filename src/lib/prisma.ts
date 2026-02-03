d979c89de1a85761af311c23f99692739e649347
Nonnzy <nonnzytransformium@gmail.com>
2026-02-03 18:22:28 +0000

    Zy Bulding workflow

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,28 @@
 import { PrismaClient } from "@prisma/client";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  // Ceci va stopper le build Vercel avec un message clair si la variable manque
+  console.error(
+    "❌ Erreur : DATABASE_URL est absente des variables d'environnement !"
+  );
+}
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl,
+      },
+    },
+  });
+};
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
