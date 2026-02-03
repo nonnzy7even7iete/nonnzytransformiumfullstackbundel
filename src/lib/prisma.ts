64f57297a51026ea5cc7fc7928d54580df964457
Nonnzy <nonnzytransformium@gmail.com>
2026-02-03 08:16:55 +0000

    Zy Bulding workflow

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  // Si on est en plein build Vercel et que DATABASE_URL est vide
-  if (!process.env.DATABASE_URL) {
-    process.env.DATABASE_URL = "mongodb://unused:unused@localhost:27017/unused";
-  }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-  return new PrismaClient();
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
