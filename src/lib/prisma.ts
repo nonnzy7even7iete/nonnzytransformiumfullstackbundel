cfac37e84750cf9bc1c67d330be4004636048b20
Nonnzy <nonnzytransformium@gmail.com>
2026-02-03 17:59:07 +0000

    CHAMPION: Revert to pure empty constructor

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-
-// Maintenant que DATABASE_URL est dans Vercel, on laisse le constructeur vide.
-// prisma.config.ts va faire le lien automatiquement.
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
