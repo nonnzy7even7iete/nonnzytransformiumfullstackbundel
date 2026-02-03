2f8afb3dbbb118d2c6e4169ae0f61579075dd8d8
Nonnzy <nonnzytransformium@gmail.com>
2026-02-03 10:55:53 +0000

    Zy Bulding workflow

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  // On récupère l'URL
+  const url =
+    process.env.DATABASE_URL ||
+    "mongodb://unused:unused@localhost:27017/unused";
+
+  // On utilise 'as any' pour que VS Code arrête de souligner 'datasourceUrl' en rouge
+  // C'est la solution pour contourner les types qui ne sont pas à jour
+  return new PrismaClient({
+    datasourceUrl: url,
+  } as any);
 };
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
