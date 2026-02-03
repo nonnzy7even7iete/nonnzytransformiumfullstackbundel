81a13024fb3b704b1aa4b75652edd81e1910dd7b
Nonnzy <nonnzytransformium@gmail.com>
2026-02-03 08:03:30 +0000

    Zy Bulding workflow

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-// On garantit à TS que c'est une string, même si c'est vide au build
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        // Si DATABASE_URL est absente (pendant le build), on met une URL bidon
-        url:
-          process.env.DATABASE_URL ||
-          "mongodb://unused:unused@localhost:27017/unused",
-      },
-    },
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasourceUrl:
+      process.env.DATABASE_URL ||
+      "mongodb://unused:unused@localhost:27017/unused",
   });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
