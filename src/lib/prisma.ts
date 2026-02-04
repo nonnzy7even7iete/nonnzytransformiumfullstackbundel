4b0690f6be9630728d8d8724c2f13b6a2e83fa52
Nonnzy <nonnzytransformium@gmail.com>
2026-02-04 16:47:07 +0000

    🚀 Deployment fix: Config bypass and Clean Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+  return new PrismaClient();
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
@@ -15,9 +10,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-// Utilise l'instance existante ou en crée une nouvelle
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-// En développement, on stocke l'instance dans le global pour éviter d'ouvrir
-// trop de connexions à chaque "Hot Reload" de Next.js
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
