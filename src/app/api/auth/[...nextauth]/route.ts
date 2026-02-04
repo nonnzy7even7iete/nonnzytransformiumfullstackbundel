fa9482a138808fb2dfb412982ccda9159ab9d559
Nonnzy <nonnzytransformium@gmail.com>
2026-02-04 16:56:15 +0000

    Zy Bulding workflow

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,8 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-// Force Next.js à ignorer cette route pendant la génération statique du build
+// Cette ligne est cruciale pour que Vercel ne crash pas au build
 export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
 
 const handler = NextAuth(authOptions);
 
